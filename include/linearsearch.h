b1f772d5c7f61868a726853ac98d2720c0a1d5c9
vscilab <vscilab@VSCI226024>
2018-09-12 14:36:11 -0700

    here goes nothing

--- include/linearsearch.h
+++ include/linearsearch.h
@@ -4,7 +4,12 @@ namespace edu { namespace vcccd { namespace vc { namespace csv15 {
 
 template <class T>
 int64_t find(const T& value, T array[], size_t size) {
-	return -1;
+	if (array == NULL) {
+		return(-1);
+	}
+	for (int i = 0; i<size; i++) {
+		if (value==array[i]) return i;
+	}return -1;
 }
 
 }}}}
--- src/main.cc
+++ src/main.cc
@@ -4,8 +4,8 @@
 using edu::vcccd::vc::csv15::find;
 
 int main(int argc, char *argv[]) {
-	int array[] = {3, 1, 2};
-	int64_t index = find(0, array, 3);
+	int array[]={3, 1, 2};
+	int64_t index=find(3, array, 3);
 	
-	std::cout << "Index: " << index << std::endl;
+	std::cout << "index: " << index << std::endl;
 }
