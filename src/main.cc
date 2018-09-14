edb8f661eb8daff7fe7a1b20dd204c415ac461eb
robertsilver2 <robert_silver2@my.vcccd.edu>
2018-09-14 16:01:29 -0700

    testttt

--- src/main.cc
+++ src/main.cc
@@ -4,7 +4,7 @@
 using edu::vcccd::vc::csv15::find;
 
 int main(int argc, char *argv[]) {
-	int array[]={3, 1, 2};
+	int array[]={1, 1, 2};
 	int64_t index=find(2, array, 3);
 	
 	std::cout << "index: " << index << std::endl;
