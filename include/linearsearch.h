6e3d3d9cd610aad7ee3210631635b709e1ba551f
robertsilver2 <robert_silver2@my.vcccd.edu>
2018-09-14 16:36:54 -0700

    Final Commit: Header message included

--- include/linearsearch.h
+++ include/linearsearch.h
@@ -1,3 +1,14 @@
+/**
+ * CS V15 Data Structures and Algorithms
+ * CRN: 70342
+ * Assignment: ex01-linearsearch
+ *
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ *
+ * @robertsilver2
+ */
+
 #include <cstdint>
 
 namespace edu { namespace vcccd { namespace vc { namespace csv15 {
